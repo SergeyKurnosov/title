a423c613fd7c8f6115a22838ef04b43d33f1d769
SergeyKurnosov <kurnosovsergej123@mail.ru>
2026-09-02 23:08:02 +1000

    classwork 02/09/2026

--- src/Main.java
+++ src/Main.java
@@ -1,224 +1,96 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-
-    /*
-    Простая
-    Дан массив из N вещественных чисел. Найти сумму, среднее арифметическое и количество элементов больше среднего.
-     */
-    double[] arr1 = {1.3, 2.1, 4.3, 9.4, 5.1, 0.2};
-    double sum = 0, arifmetic = 0;
-    int count_bigger = 0;
-    for (double elem : arr1) {
-        sum += elem;
-    }
-    arifmetic = sum / arr1.length;
-    for (double elem : arr1) {
-        if (elem > arifmetic) {
-            count_bigger++;
-        }
-    }
-    System.out.println("summ : " + sum + "\narifmetic : " + arifmetic + "\n" + "count : " + count_bigger + "\n\n\n");
-
-    //============================================================
-    /*
-    Средняя
-    Дан двумерный массив char размером M×N (заполнен буквами и цифрами).
-    Посчитать отдельно количество букв, количество цифр и количество остальных символов в каждой строке массива, вывести построчно.
-     */
-    char[][] arr2 = {
-            {'a', '3', 'e', '6', '>'},
-            {'4', 'b', '2', '!', '='},
-            {'f', '5', 'c', '1', '0'}
-    };
-
-    for (int i = 0; i < arr2.length; i++) {
-        int count_letter = 0, count_digit = 0, count_other = 0;
-        for (int j = 0; j < arr2[i].length; j++) {
-            if ((arr2[i][j] >= 'a' && arr2[i][j] <= 'z') || (arr2[i][j] >= 'A' && arr2[i][j] <= 'Z')) {
-                count_letter++;
-            } else if (arr2[i][j] >= '0' && arr2[i][j] <= '9') {
-                count_digit++;
-            } else {
-                count_other++;
-            }
-        }
-        System.out.println("=====================================================");
-        System.out.println("count letter : " + count_letter + "\ncount digit : " + count_digit + "\n count other : " + count_other + "\n\n\n");
-    }
-
-    //============================================================
-    /*
-    Сложная
-    Дан двумерный массив вещественных чисел размером N×N.
-    Для каждой строки найти индекс и значение максимального элемента.
-    Затем определить, является ли матрица симметричной относительно главной диагонали — сравнение чисел проводить с учётом погрешности (epsilon), а не через ==
-     */
-    double[][] arr3 = {
-            {1.2, 3.9, 6.1, 7.0},
-            {3.9, 3.2, 4.3, 5.2},
-            {6.1, 4.3, 1.4, 2.3},
-            {7.0, 5.2, 2.3, 0.7}
-    };
-    for (int i = 0; i < arr3.length; i++) {
-        double max_value = 0;
-        int max_index = 0;
-        for (int j = 0; j < arr3[i].length; j++) {
-            if (arr3[i][j] > max_value) {
-                max_value = arr3[i][j];
-                max_index = j;
-            }
-        }
-        System.out.println("\nmax value : " + max_value + "\nmax index : " + max_index);
-    }
-    double EPSILON = 1e-15;
-    boolean simetric = true;
-    for (int i = 0; i < arr3.length; i++) {
-        for (int j = 0; j < arr3[i].length; j++) {
-            if (Math.abs(arr3[i][j] - arr3[j][i]) > EPSILON) {
-                simetric = false;
-                break;
-            }
-        }
-        if (!simetric) {
-            break;
-        }
-    }
-    if (simetric) {
-        System.out.println("matrix symmetrical");
-
-    } else {
-        System.out.println("matrix not symmetrical");
-    }
-
-    //============================================================
-    /*
-    *
-    Дан двумерный массив char размером N×N, где буквы обозначают клетки (например, 'X', 'O', '.').
-    Реализовать проверку: существует ли в массиве сплошная линия одинаковых символов (по горизонтали, вертикали или главной/побочной диагонали) длиной не меньше заданного K, при этом символ '.' в линию не засчитывается.
-    Использовать только вложенные циклы и условия — без рекурсии и коллекций.
-     */
-    System.out.println("====================================");
+void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    System.out.println("size diagonal : ");
-    int size = scanner.nextInt();
-    char[][] arr4 = {
-            {'x', '.', '0', '0'},
-            {'0', 'x', '0', '.'},
-            {'x', '0', 'x', '0'},
-            {'0', '.', '.', '.'}
-    };
+    /*
+     1.Метод принимает пот пользовтеля возраст, возвращает boolean — подходит человек под условие или нет.
+     */
 
-    for (int i = 0; i < arr4.length; i++) {
-        boolean horizont = true;
-        boolean vertical = true;
-        boolean diagonal_down = true;
-        boolean diagonal_up = true;
-        for (int j = 0; j < arr4[i].length; j++) {
-            if (arr4[i][j] != '.') {
-                //========================================================
+    System.out.println("age : ");
+    int age = scanner.nextInt();
+    System.out.println("Adult : " + isAdult(age));
+    //========================================================================
+    /*
+    2.Создать метод который проверяет, простое ли число, используя цикл внутри метода.
+     */
+    System.out.println("num : ");
+    int num = scanner.nextInt();
+    System.out.println("Simple : " + isSimple(num));
+    //========================================================================
+    /*
+    3.Метод считает количество гласных букв в строке, перебирая символы через charAt.
+     */
+    scanner.nextLine();
+    System.out.println("line : ");
+    String line = scanner.nextLine();
+    System.out.println("vowel letters : " + countVowelLetters(line));
+    //========================================================================
+    /*
+    4.Программа в цикле принимает оценки (2–5), пока не введут слово «стоп»; невалидные оценки отклоняет, но не прерывает работу; в конце считает средний балл и выдаёт вердикт
+     */
 
-                if (horizont && j < arr4[i].length - 1) {
-                    if (arr4[i][j] == arr4[i][j + 1]) {
-                        int count_horizont = 0, last_line_index = 0;
-                        for (int k = j; k < arr4[i].length; k++) {
-                            if (arr4[i][j] != arr4[i][k]) {
-                                break;
-                            } else {
-                                count_horizont++;
-                                last_line_index = k;
-                            }
-                        }
-                        if (count_horizont <= size)
-                            System.out.println("horisont - " + arr4[i][j] + " : " + count_horizont);
-                        if (last_line_index == arr4[i].length - 1) {
-                            horizont = false;
-                        }
-                    }
-                }
-                //========================================================
-                if (vertical && i < arr4.length - 1) {
-                    if (arr4[i][j] == arr4[i + 1][j]) {
+    String grade = "";
+    String grades="";
+    String[]dates = new String[]{grade,grades};
+    do {
+        System.out.println("grade : ");
+        dates[0] = scanner.nextLine();
+        addGradeToGrades(dates);
+    }while (!dates[0].equals("стоп"));
 
-                        int count_vertical = 0, last_line_index = 0;
-                        for (int k = i; k < arr4.length; k++) {
-
-                            if (arr4[i][j] != arr4[k][j]) {
-                                break;
-                            } else {
-                                count_vertical++;
-                                last_line_index = k;
-                            }
-                        }
-                        if (count_vertical <= size)
-                            System.out.println("vertical - " + arr4[i][j] + " : " + count_vertical);
-                        if (last_line_index == arr4.length - 1) {
-                            vertical = false;
-                        }
-                    }
-                }
-                //========================================================
-                if (diagonal_down && i < arr4.length - 1 && j < arr4[i].length - 1) {
-                    if (arr4[i][j] == arr4[i + 1][j + 1]) {
-                        if (i > 0 && j > 0) {
-                            if (arr4[i][j] == arr4[i - 1][j - 1]) {
-                                continue;
-                            }
-                        }
-                        int count_diagonal_down = 0, last_line_index = 0;
-                        for (int k = 0; k < arr4.length; k++) {
-                            if (j + k == arr4[i].length || i + k == arr4.length) {
-                                break;
-                            }
-
-                            if (arr4[i][j] != arr4[i + k][j + k]) {
-                                break;
-                            } else {
-                                count_diagonal_down++;
-                                last_line_index = k;
-                            }
-                        }
-                        if (count_diagonal_down <= size)
-                            System.out.println("diagonal down - " + arr4[i][j] + " : " + count_diagonal_down);
-                        if (last_line_index == arr4.length - 1) {
-                            diagonal_down = false;
-                        }
-                    }
-                }
-                //========================================================
-                if (diagonal_up && i > 0 && j < arr4[i].length - 1) {
-                    if (arr4[i][j] == arr4[i - 1][j + 1]) {
-                        if (i > 0 && j > 0) {
-                            if (arr4[i][j] == arr4[i + 1][j - 1]) {
-                                continue;
-                            }
-                        }
-                        int count_diagonal_up = 0, last_line_index = 0;
-                        for (int k = 0; k < arr4.length; k++) {
-                            if (j + k == arr4[i].length) {
-                                break;
-                            }
-
-                            if (arr4[i][j] != arr4[i - k][j + k]) {
-                                break;
-                            } else {
-                                count_diagonal_up++;
-                                last_line_index = k;
-                            }
-                        }
-                        if (count_diagonal_up <= size)
-                            System.out.println("diagonal up - " + arr4[i][j] + " : " + count_diagonal_up);
-                        if (last_line_index == arr4.length - 1) {
-                            diagonal_up = false;
-                        }
-                    }
-                }
-            }
-
-
-        }
-    }
-
-    //============================================================
+    double middle = middleGradeToGrades(dates);
+    System.out.println("middle grade : " + middle);
 
 }
+
+private static boolean isAdult(int age){
+    if(age >= 18){
+        return true;
+    }
+    return false;
+}
+private static boolean isSimple(int num){
+    if(num == 1){
+        return false;
+    }
+    for (int i = 2; i < num; i++) {
+        if(num%i==0){
+            return false;
+        }
+    }
+
+    return true;
+}
+private static int countVowelLetters(String line){
+    int count = 0;
+    for (int i = 0; i <line.length(); i++) {
+        char toLowerchar = Character.toLowerCase(line.charAt(i));
+        if(toLowerchar == 'a' || toLowerchar == 'e' || toLowerchar == 'i' || toLowerchar == 'o' || toLowerchar == 'u'){
+            count++;
+        }
+    }
+    return count;
+}
+
+private static void addGradeToGrades(String[]dates){
+    if(!dates[0].isEmpty() && dates[0].length()==1){
+            if(!Character.isDigit(dates[0].charAt(0))){
+                return;
+            }
+            else {
+                dates[1]+=dates[0];
+            }
+    }
+}
+
+private static double middleGradeToGrades(String[]dates){
+    double result = 0.0;
+    double sum = 0;
+    for (int i = 0; i < dates[1].length(); i++) {
+        sum+=Integer.parseInt(String.valueOf(dates[1].charAt(i)));
+    }
+    result = sum / dates[1].length();
+    return result;
+}
+
+
